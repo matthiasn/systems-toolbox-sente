2b1494821b0180993844d734309fd76e38825cad
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-04-08 15:40:54 +0200

    v0.5.14: dependency upgrades

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-sente "0.5.13"
+(defproject matthiasn/systems-toolbox-sente "0.5.14"
   :description "WebSocket components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
@@ -12,7 +12,7 @@
                  [org.ow2.asm/asm-all "5.1"]
                  [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/tools.logging "0.3.1"]
-                 [io.aviso/pretty "0.1.24"]
+                 [io.aviso/pretty "0.1.25"]
                  [com.taoensso/sente "1.8.1"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [com.cognitect/transit-clj "0.8.285"]
@@ -22,7 +22,7 @@
                  [ring/ring-defaults "0.2.0"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [org.jboss.logging/jboss-logging "3.3.0.Final"]
-                 [io.undertow/undertow-core "1.3.19.Final"]
+                 [io.undertow/undertow-core "2.0.0.Alpha1"]
                  [org.immutant/web "2.1.3" :exclusions [org.jboss.xnio/xnio-api
                                                         org.jboss.logging/jboss-logging
                                                         org.jboss.xnio/xnio-nio org.slf4j/slf4j-api]]]
