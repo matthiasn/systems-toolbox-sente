f84e2630842b6a692c52cba0e4f2ad70450d3087
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-06-04 12:04:22 +0200

    v0.5.17: dependencies; no dependency on specific Clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-sente "0.5.16"
+(defproject matthiasn/systems-toolbox-sente "0.5.17"
   :description "WebSocket components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
@@ -6,9 +6,7 @@
 
   :source-paths ["src/cljc" "src/clj" "src/cljs"]
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.reader "1.0.0-beta1"]
-                 [org.clojure/clojurescript "1.8.51"]
+  :dependencies [[org.clojure/tools.reader "1.0.0-beta1"]
                  [org.ow2.asm/asm-all "5.1"]
                  [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/tools.logging "0.3.1"]
@@ -22,10 +20,13 @@
                  [ring/ring-defaults "0.2.0"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [org.jboss.logging/jboss-logging "3.3.0.Final"]
-                 [io.undertow/undertow-core "2.0.0.Alpha1"]
+                 [io.undertow/undertow-core "1.3.22.Final"]
                  [org.immutant/web "2.1.4" :exclusions [org.jboss.xnio/xnio-api
                                                         org.jboss.logging/jboss-logging
                                                         org.jboss.xnio/xnio-nio org.slf4j/slf4j-api]]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha3"]
+                                  [org.clojure/clojurescript "1.9.36"]]}}
 
   :plugins [[lein-codox "0.9.5" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.1.3"]])
