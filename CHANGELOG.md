d84a4750e926cfc8a264bf5108ec1d2fee0c6e2b
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-12-27 00:06:49 +0100

    v0.6.20: previous commit reverted; dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,15 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## [0.6.20] - 2017-12-26
+### Changed
+- previous commit reverted
+- dependencies
+
+## [0.6.19] - 2017-10-26
+### Changed
+- always relay firehose messages
+
 ## [0.6.18] - 2017-10-26
 ### Changed
 - dependencies
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-sente "0.6.18"
+(defproject matthiasn/systems-toolbox-sente "0.6.20"
   :description "WebSocket components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
@@ -8,38 +8,38 @@
 
   :dependencies
   [[org.clojure/tools.logging "0.4.0"]
-   [com.taoensso/sente "1.11.0" :exclusions [org.clojure/core.async
+   [com.taoensso/sente "1.12.0" :exclusions [org.clojure/core.async
                                              org.clojure/tools.reader]]
    [org.immutant/web "2.1.9" :exclusions [ring/ring-core]]
    [org.clojure/core.match "0.3.0-alpha4" :exclusions [org.clojure/tools.analyzer.jvm
                                                        org.clojure/core.memoize]]
    [com.cognitect/transit-clj "0.8.300"]
    [com.cognitect/transit-cljs "0.8.243"]
-   [ring "1.6.2"]
+   [ring "1.6.3"]
    [compojure "1.6.0"]
    [ring/ring-defaults "0.3.1"]
    [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
    [org.jboss.logging/jboss-logging "3.3.1.Final"]]
 
   :profiles
-  {:dev {:dependencies   [[org.clojure/clojure "1.9.0-beta3"]
+  {:dev {:dependencies   [[org.clojure/clojure "1.9.0"]
                           [org.clojure/clojurescript "1.9.946"]
-                          [re-frame "0.10.1" :exclusions [org.clojure/tools.reader]]
-                          [matthiasn/systems-toolbox "0.6.23"
+                          [re-frame "0.10.2" :exclusions [org.clojure/tools.reader]]
+                          [matthiasn/systems-toolbox "0.6.27"
                            :exclusions [org.clojure/tools.reader]]
                           [ch.qos.logback/logback-classic "1.2.3"]
                           [hiccup "1.0.5"]
-                          [org.seleniumhq.selenium/selenium-java "3.6.0"
+                          [org.seleniumhq.selenium/selenium-java "3.8.1"
                            :exclusions [org.seleniumhq.selenium/selenium-support
                                         org.seleniumhq.selenium/selenium-api
                                         org.seleniumhq.selenium/selenium-support]]
-                          [org.seleniumhq.selenium/selenium-api "3.6.0"]
-                          [org.seleniumhq.selenium/selenium-server "3.6.0"
+                          [org.seleniumhq.selenium/selenium-api "3.8.1"]
+                          [org.seleniumhq.selenium/selenium-server "3.8.1"
                            :exclusions [org.seleniumhq.selenium/selenium-support
                                         org.seleniumhq.selenium/selenium-api]]
-                          [org.seleniumhq.selenium/selenium-support "3.6.0"]
-                          [org.seleniumhq.selenium/selenium-remote-driver "3.6.0"]
-                          [org.seleniumhq.selenium/selenium-chrome-driver "3.6.0"]
+                          [org.seleniumhq.selenium/selenium-support "3.8.1"]
+                          [org.seleniumhq.selenium/selenium-remote-driver "3.8.1"]
+                          [org.seleniumhq.selenium/selenium-chrome-driver "3.8.1"]
                           [com.codeborne/phantomjsdriver "1.4.3"
                            :exclusions [org.apache.httpcomponents/httpcore]]
                           [clj-webdriver "0.7.2"
